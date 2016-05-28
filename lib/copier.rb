1fd4414865bdf550547dfe42a2cbd0c754bdb3c9
fathergoose <al@ilseman.com>
2016-05-28 10:09:40 -0500

    Copier#new assigns arguments as directories

--- lib/copier.rb
+++ lib/copier.rb
@@ -1,9 +1,11 @@
+require 'fileutils'
+
 class Copier
   attr_reader :source_dir, :destination_dir
 
   def initialize(source_dir, destination_dir)
-    @source_dir = source_dir
-    @destination_dir = destination_dir
+    @source_dir = Dir.new(source_dir)
+    @destination_dir = Dir.new(destination_dir)
   end
 
 end
--- spec/copier_spec.rb
+++ spec/copier_spec.rb
@@ -5,5 +5,16 @@ RSpec.describe Copier do
     it "takes two argmuents" do 
       expect(Copier).to respond_to(:new).with(2).arguments
     end
+    it "assigns the arguments as directories" do
+      copier = Copier.new('./', '../')
+      src_dir = copier.source_dir
+      dst_dir = copier.destination_dir
+      expect(src_dir).to be_a(Dir) 
+      expect(dst_dir).to be_a(Dir)
+    end
   end
+
+  describe ".list" do
+  end
+
 end
