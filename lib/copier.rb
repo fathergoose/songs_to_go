22b5e41b1fab1c542996bd851bdc6a01c5397d82
fathergoose <al@ilseman.com>
2016-05-28 14:02:18 -0500

    super basic MVP

--- lib/copier.rb
+++ lib/copier.rb
@@ -2,10 +2,10 @@ require 'pathname'
 
 class Copier
   attr_reader :source_path, :destination_path
-
+  
   def initialize(source_dir, destination_dir)
     @source_path = Pathname.new(source_dir)
-    @destinantion_path = Pathname.new(destination_dir)
+    @destination_path = Pathname.new(destination_dir)
   end
 
   def sources
